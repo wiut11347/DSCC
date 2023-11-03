6f684a357dc8b89c138c4acf24c75292c314b06e
00011347 <00011347@wiut.uz>
2023-11-04 01:53:08 +0500

    init: project

---
+++ WAD_00011347/Models/Bike.cs
@@ -0,0 +1,12 @@
+﻿namespace WAD_00011347.Models
+{
+    public class Bike
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+        public decimal Price { get; set; }
+        public BikeCategory Category { get; set; }
+    }
+}
